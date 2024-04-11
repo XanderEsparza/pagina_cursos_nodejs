b8daafe5b333cf187e5ca241c2f9ec6cf44a3eb1
XanderEsparza <2136000805@utna.edu.mx>
2024-04-11 14:20:26 -0600

    modifed models

--- migrations/20240402015616-create-purchase.js
+++ migrations/20240402015616-create-purchase.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Purchases', {
+    await queryInterface.createTable('purchases', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -44,6 +44,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Purchases');
+    await queryInterface.dropTable('purchases');
   }
 };
